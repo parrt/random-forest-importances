81529f4364b643d43c4255afec7d83dba75463c4
parrt <parrt@cs.usfca.edu>
2018-03-22 13:18:35 -0700

    start adding code to article

--- article/article.md
+++ article/article.md
@@ -1,3 +1,6 @@
 # Default Random Forest Importances Are Inaccurate
 
-testing
+<pyeval label=rent output="df.head(3) # dump first 3 rows">
+import pandas as pd
+df = pd.read_csv("data/rent.csv")
+</pyeval>
--- bin/render-html.sh
+++ bin/render-html.sh
@@ -7,6 +7,7 @@ while true
 do
 	if test $I/article.md -nt $O/article.html
 	then
+		echo "bookish -target html -o $O $I/article.json"
 		java -jar /Users/parrt/github/bookish/target/bookish-1.0-SNAPSHOT.jar -target html -o $O $I/article.json
 	fi
 	sleep .2s
