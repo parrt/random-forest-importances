1f7999c4a03f427b4c1ba3ab95b581ee291e6cc8
parrt <parrt@cs.usfca.edu>
2018-03-23 13:58:13 -0700

    add separate python file

---
+++ notebooks/rfperm.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.base import clone
+from sklearn.metrics import r2_score
+import warnings
+
+from sklearn.ensemble.forest import _generate_unsampled_indices
+
+
+def oob_classifier_accuracy(rf, X_train, y_train):
+    X = X_train.values
+    y = y_train.values
+
+    n_samples = len(X)
+    n_classes = len(np.unique(y))
+    predictions = np.zeros((n_samples, n_classes))
+    for tree in rf.estimators_:
+        unsampled_indices = _generate_unsampled_indices(tree.random_state, n_samples)
+        tree_preds = tree.predict_proba(X[unsampled_indices, :])
+        predictions[unsampled_indices] += tree_preds
+
+    predicted_class_indexes = np.argmax(predictions, axis=1)
+    predicted_classes = [rf.classes_[i] for i in predicted_class_indexes]
+
+    oob_score = np.mean(y == predicted_classes)
+    return oob_score
+
+
+def oob_regression_r2_score(rf, X_train, y_train):
+    X = X_train.values
+    y = y_train.values
+
+    n_samples = len(X)
+    n_classes = len(np.unique(y))
+    predictions = np.zeros(n_samples)
+    n_predictions = np.zeros(n_samples)
+    for tree in rf.estimators_:
+        unsampled_indices = _generate_unsampled_indices(tree.random_state, n_samples)
+        tree_preds = tree.predict(X[unsampled_indices, :])
+        predictions[unsampled_indices] += tree_preds
+        n_predictions[unsampled_indices] += 1
+
+    if (n_predictions == 0).any():
+        warnings.warn("Too few trees; some variables do not have OOB scores.")
+        n_predictions[n_predictions == 0] = 1
+
+    predictions /= n_predictions
+    
+    oob_score = r2_score(y, predictions)
+    return oob_score
+
+
+def permutation_importances(rf, X_train, y_train, metric):
+    """
+    Return importances from pre-fit rf; metric is function
+    that measures accuracy or R^2 or similar. This function
+    works for regressors and classifiers.
+    """
+    baseline = metric(rf, X_train, y_train)
+    imp = []
+    for col in X_train.columns:
+        save = X_train[col].copy()
+        X_train[col] = np.random.permutation(X_train[col])
+        m = metric(rf, X_train, y_train)
+        X_train[col] = save
+        imp.append(baseline - m)
+    return np.array(imp)
