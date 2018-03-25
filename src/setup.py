b3ab5075b1582390bb469c4d9090ef0177a38be8
parrt <parrt@cs.usfca.edu>
2018-03-25 10:20:21 -0700

    rename package

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='rfimpo',
+    name='rfpimp',
     version='1.0',
     url='https://github.com/parrt/random-forest-importances',
     license='MIT',
-    py_modules=['rfimpo'],
+    py_modules=['rfpimp'],
     author='Terence Parr, Kerem Turgutlu',
     author_email='parrt@antlr.org, kcturgutlu@dons.usfca.edu',
     install_requires=['numpy','pandas','sklearn','matplotlib'],
     description='Permutation and drop-column importance for scikit-learn random forests',
-    keywords='scikit-learn random forest feature importances',
+    keywords='scikit-learn random forest feature permutation importances',
     classifiers=['License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers']
 )
