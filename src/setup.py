9923a38e721ab3f66d6ee8eb63a8e629e43377ee
parrt <parrt@cs.usfca.edu>
2018-03-29 11:20:16 -0700

    tweak readme

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rfpimp',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/parrt/random-forest-importances',
     license='MIT',
     py_modules=['rfpimp'],
