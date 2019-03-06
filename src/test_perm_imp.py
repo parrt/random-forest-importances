377359c7ae92ff081fd6b03158826c8066dfa5e2
parrt <parrt@cs.usfca.edu>
2019-03-05 16:11:17 -0800

    tweak test routine

--- src/test_perm_imp.py
+++ src/test_perm_imp.py
@@ -8,6 +8,8 @@ from pandas.api.types import is_string_dtype, is_numeric_dtype, is_object_dtype,
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.base import clone
+import cProfile, pstats, io
+from pstats import SortKey
 
 from timeit import default_timer as timer
 
@@ -21,12 +23,28 @@ rf = RandomForestRegressor(n_estimators=50,
                            max_features=.4)
 X_train, y_train = df.drop('SalePrice', axis=1), df['SalePrice']
 
+print("Data loaded")
+
+rf.fit(X_train, y_train)
+
+print("Model fit")
+
 start = timer() # ------------
 
-I = oob_importances(rf, X_train, y_train, n_samples=3000)
+#I = oob_importances(rf, X_train, y_train, n_samples=3000)
+profiler = cProfile.Profile()
+profiler.enable()
+I = importances(rf, X_train, y_train, n_samples=3000)
+profiler.disable()
 
 end = timer() # ------------
 print(f"{end - start:.2f}s")
+
+s = io.StringIO()
+sortby = SortKey.TIME
+ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
 
 viz = plot_importances(I)
 viz.view()
