95cfa622de63093cf9bfbd53dc0a7b0395fc1134
parrt <parrt@cs.usfca.edu>
2019-04-23 09:04:52 -0700

    refactor

--- src/varcontrol_lm.py
+++ src/varcontrol_lm.py
@@ -58,23 +58,7 @@ def beta_hp(X, y):
     # Predict mpg using wgt using hp
     r_y_wgt_res = LinearRegression()
     r_y_wgt_res.fit(res_hp.values.reshape(-1,1), y)
-    return r_y_wgt_res.coef_
-
-
-def beta_hp_rf(X, y) -> RandomForestRegressor:
-    # Predict hp using wgt
-    rf_wgt_hp = RandomForestRegressor(n_estimators=100)
-    rf_wgt_hp.fit(X[['WGT']], X['ENG'])
-    # Residual of true wgt might prediction based upon hp
-    # res_hp is the variation in hp that can't be explained by wgt
-    res_hp  = hp - rf_wgt_hp.predict(wgt.values.reshape(-1,1))
-    print(pd.concat([X,res_hp,y], axis=1).head(50))
-
-    # Predict mpg using wgt using hp
-    r_y_wgt_res = RandomForestRegressor(n_estimators=100)
-    r_y_wgt_res.fit(res_hp.values.reshape(-1,1), y)
-    # return res_hp.unique(), r_y_wgt_res.predict(res_hp.unique().reshape(-1,1))
-    return res_hp, r_y_wgt_res.predict(res_hp.values.reshape(-1,1))
+    return r_y_wgt_res.coef_, r_y_wgt_res.intercept_, res_hp
 
 
 def beta_wgt(X,y):
@@ -90,22 +74,7 @@ def beta_wgt(X,y):
     # Predict mpg using wgt using hp
     rf_y_hp_res = LinearRegression()
     rf_y_hp_res.fit(res_wgt.values.reshape(-1,1), y)
-    return rf_y_hp_res.coef_
-
-
-def beta_wgt_rf(X,y) -> np.ndarray:
-    # Predict wgt using hp
-    rf_hp_wgt = RandomForestRegressor(n_estimators=100)
-    rf_hp_wgt.fit(X[['ENG']], X['WGT'])
-    # Residual of true wgt might prediction based upon hp
-    # res_wgt is the variation in wgt that can't be explained by hp
-    res_wgt = wgt - rf_hp_wgt.predict(hp.values.reshape(-1,1))
-
-    # Predict mpg using wgt using hp
-    rf_y_hp_res = RandomForestRegressor(n_estimators=100)
-    rf_y_hp_res.fit(res_wgt.values.reshape(-1,1), y)
-    # return res_wgt.unique(), rf_y_hp_res.predict(res_wgt.unique().reshape(-1,1))
-    return res_wgt, rf_y_hp_res.predict(res_wgt.values.reshape(-1,1))
+    return rf_y_hp_res.coef_, rf_y_hp_res.intercept_, res_wgt
 
 
 if __name__ == '__main__':
@@ -131,20 +100,24 @@ if __name__ == '__main__':
     print("\nRegression on wgt predicting mpg")
     print(f"mpg_wgt = {r_wgt.coef_}*wgt + {r_wgt.intercept_}")
 
-    print(f"Beta for hp is {beta_hp(X,y)} (see mpg coeff[1])")
+    b_hp, int_hp, res_hp = beta_hp(X, y)
+    print(f"Beta for hp is {b_hp} (see mpg coeff[1])")
     # print(f"Beta via RF for hp is {beta_hp_rf(X,y)} (see mpg coeff[1])")
 
-    print(f"Beta for wgt is {beta_wgt(X,y)} (see mpg coeff[1])")
+    b_wgt, int_wgt, res_wgt = beta_wgt(X, y)
+    print(f"Beta for wgt is {b_wgt} (see mpg coeff[1])")
     # print(f"Beta via RF for wgt is {beta_wgt_rf(X,y)} (see mpg coeff[1])")
 
-    fig,axes = plt.subplots(3,2,sharey=False)
+    fig,axes = plt.subplots(3,2,sharey=False,figsize=(7.5,9))
     tl = axes[0,0]
     tr = axes[0,1]
-    tl.scatter(X['ENG'],y,alpha=.25)
-    tr.scatter(X['WGT'],y,c='blue',alpha=.2)
+    tl.scatter(X['ENG'],y,alpha=.12)
+    tr.scatter(X['WGT'],y,c='blue',alpha=.08)
     tl.set_xlabel('Horsepower')
     tr.set_xlabel('Weight')
     tl.set_ylabel('MPG')
+    tl.set_title('MPG vs hp')
+    tr.set_title('MPG vs wgt')
 
     xhp = np.arange(0,max(hp),1)
     xwgt = np.arange(0,max(wgt),1)
@@ -152,22 +125,51 @@ if __name__ == '__main__':
     xwgt = np.arange(min(wgt),max(wgt),1)
 
     # Show biased simple y=x regressions
-    tl.plot(xhp, r_hp.predict(xhp.reshape(-1,1)), ":", linewidth=1.5, c='black')
-    tr.plot(xwgt,r_wgt.predict(xwgt.reshape(-1,1)), ":", linewidth=1.5, c='black')
-
-    # Show RF derived partial dep
-    res_hp,foo = beta_hp_rf(X, y)
-    axes[1,0].scatter(X['ENG'],res_hp, linewidth=1, s=3, c='green')
-    res_wgt,foo = beta_wgt_rf(X, y)
-    axes[1,1].scatter(X['WGT'], res_wgt, linewidth=1, s=3, c='green')
-
-    res_hp,foo = beta_hp_rf(X, y)
-    axes[2,0].scatter(res_hp, y, linewidth=1, s=3, c='green')
-    res_wgt,foo = beta_wgt_rf(X, y)
-    axes[2,1].scatter(res_wgt, y, linewidth=1, s=3, c='green')
+    y_pred_hp  = r_hp.predict(hp.values.reshape(-1, 1))
+    y_pred_wgt = r_wgt.predict(wgt.values.reshape(-1, 1))
+    tl.plot(hp,  y_pred_hp,  linewidth=1, c='black', label='OLS y ~ hp')
+    tr.plot(wgt, y_pred_wgt, linewidth=1, c='black', label='OLS y ~ wgt')
 
     # Show partial dependencies from regression coeff
-    tl.plot(xhp,xhp*r.coef_[0], linewidth=1, c='orange')
-    tr.plot(xwgt,xwgt*r.coef_[1], linewidth=1, c='orange')
+    # tl.plot(xhp,  xhp*r.coef_[0],  linewidth=1, c='orange')
+    # tr.plot(xwgt, xwgt*r.coef_[1], linewidth=1, c='orange')
 
+    # Show residuals from OLS
+    y_res_hp  = y - y_pred_hp
+    y_res_wgt = y - y_pred_wgt
+    # axes[1,0].scatter(hp,  y_res_hp,  linewidth=1, s=3, c='green')
+    # axes[1,1].scatter(wgt, y_res_wgt, linewidth=1, s=3, c='green')
+    # Show diff between two predicted models
+    axes[1,0].scatter(hp,  r.predict(X)-y_pred_hp, linewidth=1, s=3, c='purple')
+    axes[1,1].scatter(wgt, r.predict(X)-y_pred_wgt, linewidth=1, s=3, c='purple')
+    axes[1,0].set_title('Residual of y ~ hp+wgt and y ~ hp')
+    axes[1,1].set_title('Residual of y ~ hp+wgt and y ~ wgt')
+    axes[1,0].set_xlabel('Horsepower')
+    axes[1,1].set_xlabel('Weight')
+
+    # Show residuals of hp - predicted hp using wgt and vice versa
+    axes[2,0].scatter(hp,  res_hp,  linewidth=1, s=3, c='purple')
+    axes[2,1].scatter(wgt, res_wgt, linewidth=1, s=3, c='purple')
+    axes[2,0].set_title('Residual of hp and hp ~ wgt')
+    axes[2,1].set_title('Residual of wgt and wgt ~ hp')
+    axes[2,0].set_xlabel('Horsepower')
+    axes[2,1].set_xlabel('Weight')
+
+    if False:
+        # Show residuals of hp and wgt vs y
+        axes[3,0].scatter(res_hp,  y, linewidth=1, s=3, c='green')
+        axes[3,1].scatter(res_wgt, y, linewidth=1, s=3, c='green')
+        axes[3,0].set_xlabel('Residual of hp and hp ~ wgt')
+        axes[3,1].set_xlabel('Residual of wgt and wgt ~ hp')
+
+        # Draw OLS through residual hp predicting y and then for wgt
+        axes[3,0].scatter(res_hp,  res_hp * b_hp + int_hp,    s=1, c='orange')
+        axes[3,1].scatter(res_wgt, res_wgt * b_wgt + int_wgt, s=1, c='orange')
+
+    tl.legend()
+    tr.legend()
+
+    plt.tight_layout()
+
+    plt.savefig("/tmp/t.svg", tight_layout=True)
     plt.show()
