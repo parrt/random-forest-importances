311ce63e1df239c84aae1b26af27e663db0993e5
parrt <parrt@cs.usfca.edu>
2019-04-24 11:49:45 -0700

    work on samples per leaf

--- src/varcontrol.py
+++ src/varcontrol.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import load_boston, load_iris, load_wine, load_digits, \
+    load_breast_cancer, load_diabetes, fetch_mldata
+
 
 """
 Play with controlling for other variables to see contribution of some x to y
@@ -23,7 +26,19 @@ def foo(rf, X, y):
         right = t.tree_.children_right
         print(nnodes)
         for n in range(nnodes):
+            pass
 
+
+def df_scramble(X : pd.DataFrame) -> pd.DataFrame:
+    """
+    From Breiman: https://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm
+    "...the first coordinate is sampled from the N values {x(1,n)}. The second
+    coordinate is sampled independently from the N values {x(2,n)}, and so forth."
+    """
+    X_rand = X.copy()
+    for colname in X:
+        X_rand[colname] = np.random.choice(X[colname].unique(), len(X), replace=True)
+    return X_rand
 
 
 # From dtreeviz
@@ -46,15 +61,34 @@ def node_samples(tree_model, X) -> Mapping[int, list]:
     return node_to_samples
 
 
-if __name__ == '__main__':
-    df_cars = pd.read_csv("/Users/parrt/github/dtreeviz/testing/data/cars.csv")
-    X = df_cars[['ENG','WGT']]
-    y = df_cars['MPG']
+def wine():
+    wine = load_wine()
 
+
+def cars():
+    df_cars = pd.read_csv("/Users/parrt/github/dtreeviz/testing/data/cars.csv")
+    X = df_cars[['ENG', 'WGT']]
+    y = df_cars['MPG']
     hp = X['ENG']
     wgt = X['WGT']
-
-    rf = RandomForestRegressor(n_estimators=1, min_samples_leaf=5)
-    rf.fit(X, y)
-
+    # Make new data set 2x as big with X and scrambled version of it
+    # that destroys structure between features. Old is class 0, scrambled
+    # is class 1.
+    X_synth, y_synth = conjure_twoclass(X)
+    # Fit an RF on this new concat'd dataset
+    rf = RandomForestRegressor(n_estimators=1, min_samples_leaf=5, oob_score=True)
+    rf.fit(X_synth, y_synth)
+    print(f"OOB R^2 {rf.oob_score_:.5f}")
     foo(rf, X, y)
+
+
+def conjure_twoclass(X):
+    X_rand = df_scramble(X)
+    X_synth = pd.concat([X, X_rand], axis=0)
+    y_synth = np.concatenate([np.zeros(len(X)),
+                              np.ones(len(X_rand))], axis=0)
+    return X_synth, y_synth
+
+
+if __name__ == '__main__':
+    wine()
