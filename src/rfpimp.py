01beefb12b2ece4fc1b439da834d93db580eae55
Baldino <Michael.Baldino@lfd.com>
2020-06-07 12:19:18 -0400

    Add oob_classifier_f1_score

--- src/rfpimp.py
+++ src/rfpimp.py
@@ -18,6 +18,7 @@ from sklearn.ensemble import forest
 from sklearn.model_selection import cross_val_score
 from sklearn.base import clone
 from sklearn.metrics import r2_score
+from sklearn.metrics import f1_score
 from scipy.stats import spearmanr
 from pandas.api.types import is_numeric_dtype
 from matplotlib.colors import ListedColormap
@@ -450,6 +451,32 @@ def oob_classifier_accuracy(rf, X_train, y_train):
     predicted_classes = [rf.classes_[i] for i in predicted_class_indexes]
 
     oob_score = np.mean(y == predicted_classes)
+    return oob_score
+
+
+def oob_classifier_f1_score(rf, X_train, y_train):
+    """
+    Compute out-of-bag (OOB) f1 score for a scikit-learn random forest
+    classifier. We learned the guts of scikit's RF from the BSD licensed
+    code:
+
+    https://github.com/scikit-learn/scikit-learn/blob/a24c8b46/sklearn/ensemble/forest.py#L425
+    """
+    X = X_train.values
+    y = y_train.values
+
+    n_samples = len(X)
+    n_classes = len(np.unique(y))
+    predictions = np.zeros((n_samples, n_classes))
+    for tree in rf.estimators_:
+        unsampled_indices = _get_unsampled_indices(tree, n_samples)
+        tree_preds = tree.predict_proba(X[unsampled_indices, :])
+        predictions[unsampled_indices] += tree_preds
+
+    predicted_class_indexes = np.argmax(predictions, axis=1)
+    predicted_classes = [rf.classes_[i] for i in predicted_class_indexes]
+
+    oob_score = f1_score(y, predicted_classes, average='weighted')
     return oob_score
 
 
